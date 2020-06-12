18952f738cfbbd6d8261f92b2fa2d3357a60fb47
Eliseu Codinhoto <zeucxb@gmail.com>
2020-06-12 00:11:10 -0300

    add socket.io integration

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const port = 3000;
+
+var lastData = null;
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
+  extended: true
 }));
 
-app.post('/hello', function (req, res) {
-    console.log(req.body);
-    res.send('Hello World!');
+function sendLastData() {
+  io.sockets.emit(lastData);
+}
+
+app.post('/', function (req, res) {
+  lastData = req.body;
+  sendLastData();
+  res.sendStatus(200);
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+io.on('connection', (socket) => {
+  sendLastData();
+});
+
+http.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
